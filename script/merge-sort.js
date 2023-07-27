51ffd52325d7e363218180707f0d6e8bd1a87e17
Pasan Kavinda Abeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2023-07-27 19:10:35 +0530

    Create merge-sort.js

---
+++ script/merge-sort.js
@@ -0,0 +1,35 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let mergedArr = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      mergedArr.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      mergedArr.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // Concatenate any remaining elements from left and right arrays
+  return mergedArr.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+// Example usage:
+const unsortedArray = [38, 27, 43, 3, 9, 82, 10];
+const sortedArray = mergeSort(unsortedArray);
+console.log(sortedArray);
